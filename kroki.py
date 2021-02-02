409918ede206a10d77acf75cbdc6915be5ad0f26
Bartek W <bewuwy@gmail.com>
2021-02-02 13:25:43 +0100

    saving old data to json file renamed some variables

--- kroki.py
+++ kroki.py
@@ -4,6 +4,7 @@ from os.path import exists
 from time import time_ns
 from datetime import datetime, timedelta
 import plotly.graph_objects as go
+import json
 import requests
 import pickle
 
@@ -39,34 +40,44 @@ if dataStreamId is None:
     quit()
 
 
-now = datetime.today()
-week = datetime(now.year, now.month, now.day, now.hour, now.minute) - timedelta(days=7)
-week = int((week-datetime(1970, 1, 1)).total_seconds() * (10**9))
-now = int(time_ns())
+end = datetime.today()
+start = datetime(end.year, end.month, end.day, end.hour, end.minute) - timedelta(days=7)
+start = int((start-datetime(1970, 1, 1)).total_seconds() * (10**9))
+end = int(time_ns())
 
-datasetId = f"{str(week)}-{str(now)}"
+datasetId = f"{str(start)}-{str(end)}"
 r = requests.get(f"https://fitness.googleapis.com/fitness/v1/users/me/dataSources/{dataStreamId}/datasets/{datasetId}",
                  headers=headers)
 steps = r.json()
 
-steps_dict = {}
+if exists("data.json"):
+    with open("data.json", "r") as f:
+        steps_data = json.load(f)
+else:
+    steps_data = {}
+
 for i in steps["point"]:
     start = (datetime.fromtimestamp(int(i["startTimeNanos"])/(10**9)))
-    # stop = (datetime.fromtimestamp(int(i["endTimeNanos"])/(10**9)))
+    steps_data[str(start.date())] = 0
+for i in steps["point"]:
+    start = (datetime.fromtimestamp(int(i["startTimeNanos"])/(10**9)))
+    # end = (datetime.fromtimestamp(int(i["endTimeNanos"])/(10**9)))
     value = i["value"][0]["intVal"]
 
-    if str(start.date()) in steps_dict:
-        steps_dict[str(start.date())] += value
+    if str(start.date()) in steps_data:
+        steps_data[str(start.date())] += value
     else:
-        steps_dict[str(start.date())] = value
-    # print(f"{start} - {stop} -> {value}")
+        steps_data[str(start.date())] = value
+    # print(f"{start} - {end} -> {value}")
 
-print(steps_dict, end="\n"*2)
-for i in steps_dict:
-    print(f"{i} -> {steps_dict[i]}")
+print(steps_data, end="\n"*2)
+for i in steps_data:
+    print(f"{i} -> {steps_data[i]}")
 
+with open("data.json", "w") as f:
+    json.dump(steps_data, f, indent=4)
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=list(steps_dict.keys()), y=list(steps_dict.values()), name="steps"))
+fig.add_trace(go.Scatter(x=list(steps_data.keys()), y=list(steps_data.values()), name="steps"))
 fig.update_layout(title="daily steps", xaxis_title="date", yaxis_title="steps")
 fig.show()
