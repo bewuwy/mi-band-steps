a0688e594bcc8daf57e3677c7debe800a7d3a221
Bartek W <bewuwy@gmail.com>
2021-02-02 10:13:13 +0100

    changed from googleapiclient.discovery to requests changed now to utc time changed the dataset from one day to a week experimented with aggregate

--- kroki.py
+++ kroki.py
@@ -1,9 +1,10 @@
-from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from os.path import exists
 from time import time_ns
-from datetime import datetime
+from datetime import datetime, timedelta
+# from json import dumps
+import requests
 import pickle
 
 
@@ -17,35 +18,56 @@ if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
         creds.refresh(Request())
     else:
-        flow = InstalledAppFlow.from_client_secrets_file('token/credentials.json.json', scopes)
+        flow = InstalledAppFlow.from_client_secrets_file('token/credentials.json', scopes)
         creds = flow.run_local_server()
     with open('token/token.pickle', 'wb') as token:
         pickle.dump(creds, token)
 
 
-service = build('fitness', 'v1', credentials=creds)
+headers = {"Authorization": f"Bearer {creds.token}", "Content-Type": "application/json;encoding=utf-8"}
+r = requests.get("https://fitness.googleapis.com/fitness/v1/users/me/dataSources", headers=headers)
 
-data_sources = service.users().dataSources().list(userId='me').execute()
+data_sources = r.json()
 
 dataStream = None
 for i in data_sources["dataSource"]:
-    if i['dataStreamName'] == "kroki.bewuwy":
-        print(i)
+    if i['dataStreamId'] == "raw:com.google.step_count.delta:com.xiaomi.hm.health:":
         dataStream = i
         break
-
 if dataStream is None:
     print("could not find the dataStream")
     quit()
 
-
 dataStreamId = dataStream["dataStreamId"]
 
-now = datetime.now()
-today = datetime(now.year, now.month, now.day, 0, 0)
-today = int((today-datetime(1970, 1, 1)).total_seconds() * (10**9))
-datasetId = f"{str(today)}-{str(time_ns())}"
+now = datetime.utcnow()
+week = datetime(now.year, now.month, now.day, now.hour, now.minute) - timedelta(days=7)
+week = int((week-datetime(1970, 1, 1)).total_seconds() * (10**4))
+now = int(time_ns() / (10**5))
 
-dataset = service.users().dataSources().datasets(). \
-                get(userId='me', dataSourceId=dataStreamId, datasetId=datasetId).execute()
-print(dataset)
+# # aggregate
+# body = dumps({"aggregateBy": [
+#              {"dataTypeName": "com.google.step_count.delta",
+#               "dataSourceId": dataStreamId}],
+#               "bucketByTime": {"durationMillis": 86400000},
+#               "startTimeMillis": week, "endTimeMillis": now})
+#
+# r = requests.post("https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate", headers=headers, data=body)
+#
+# steps = r.json()
+# for i in steps["bucket"]:
+#     print(i)
+#
+# print("\n\n\n")
+
+
+# dataset
+datasetId = f"{str(week*(10**5))}-{str(now*(10**5))}"
+
+r = requests.get(f"https://fitness.googleapis.com/fitness/v1/users/me/dataSources/{dataStreamId}/datasets/{datasetId}",
+                 headers=headers)
+steps = r.json()
+
+print(steps)
+for i in steps["point"]:
+    print(i)
